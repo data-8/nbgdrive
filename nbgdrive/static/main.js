90ac12e9c338cb9086bca8e882f4580095b19906
Jeff Gong <jgong@berkeley.edu>
2016-11-22 01:26:45 -0800

    Bump version, add new icon, add autosync every 24 hours at 3 AM

--- nbgdrive/static/main.js
+++ nbgdrive/static/main.js
@@ -7,32 +7,47 @@ define([
 ) {
     function createDisplayDiv() {
         $('#maintoolbar-container').append(
-            $('<div>').attr('id', 'nbresuse-display')
+            $('<div>').attr('id', 'nbgdrive-display')
                       .addClass('btn-group')
                       .addClass('pull-right')
             .append(
                 $('<strong>').text('Creating Google Drive Directory: ')
             ).append(
-                $('<span>').attr('id', 'nbresuse-mem')
-                           .attr('title', 'Actively used Memory (updates every 5s)')
+                $('<span>').attr('id', 'nbgdrive-status')
+                           .attr('title', 'Latest action performed by GDrive Extension')
             )
         );
     }
 
-    var displayMetrics = function() {
-        $.getJSON(utils.get_body_data('baseUrl') + 'gdrive', function(data) {
-            // FIXME: Proper setups for MB and GB. MB should have 0 things
-            // after the ., but GB should have 2.
+    var autosync_gdrive_files = function () {
+        $.getJSON(utils.get_body_data('baseUrl') + 'gsync', function(data) {
             var display = String(data['status']);
             console.log ("Current text: " + display);
-            $('#nbresuse-mem').text(display);
+            $('#nbgdrive-status').text(display);
         });
+    }
+
+    var check_autosync_time = function () {
+        console.log ("Checking the time for autosync.");
+        var date = new Date();
+        if (date.getHours() === 3  && date.getMinutes() === 0) {
+            autosync_gdrive_files();
+        }
     }
 
     var load_ipython_extension = function () {
         /* Creates an extra field for the Jupyter notebook. */
         createDisplayDiv();
-        displayMetrics();
+        
+        /* Triggers the directory to be created a single time. */
+        $.getJSON(utils.get_body_data('baseUrl') + 'gdrive', function(data) {
+            var display = String(data['status']);
+            $('#nbgdrive-status').text(display);
+        });
+
+        /* Create a function that checks the time every minute, autosyncs when 3 AM
+         * TODO: EXTREMELY HACKY. */
+        setInterval(check_autosync_time, 1000 * 60);
 
         /* Registers a new button with the notebook. */
         var handler = function () {
@@ -41,12 +56,12 @@ define([
                 // after the ., but GB should have 2.
                 var display = String(data['status']);
                 console.log ("Current text: " + display);
-                $('#nbresuse-mem').text(display);
+                $('#nbgdrive-status').text(display);
             });
         }
 
         var action = {
-            icon: 'fa-comment-o', // a font-awesome class used on buttons, etc
+            icon: 'fa-cloud', // a font-awesome class used on buttons, etc
             help    : 'Manually syncs the home directory with Google Drive',
             help_index : 'zz',
             handler : handler
