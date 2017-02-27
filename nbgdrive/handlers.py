026202ed1f752c8f93d1cf93e07abdc88ee212ce
Micah Carroll <micah.d.carroll@berkeley.edu>
2017-02-26 16:15:58 -0800

    Removed 'jovyan' from path in synced Google Drive folder

--- nbgdrive/handlers.py
+++ nbgdrive/handlers.py
@@ -1,4 +1,4 @@
-import os
+
 import json
 from subprocess import Popen, PIPE
 from notebook.utils import url_path_join
@@ -8,8 +8,11 @@ from notebook.base.handlers import IPythonHandler
 # They are stated below:
 
 # 1. When running on each singleuser, there is a config variable called JPY_USER with their unique identification
-# 3. We will allow this extension to create a sync directory from the single user's home directory to data8/$JPY_USER
-# 4. File deletes locally are not syncable to the sync directory, this is a bug we must fix
+# 2. We will allow this extension to create a sync directory from the single user's home directory to data8/$JPY_USER
+# 3. File deleted locally are not syncable to the sync directory, this is a bug we must fix
+# 4. The /home directory contains just one folder, called `jovyan`, which is the standard student working directory
+
+HOME_SUBFOLDER_NAME = "jovyan"
 
 def make_gdrive_directory():
     drive_authenticated = check_gdrive_authenticated()
@@ -19,15 +22,15 @@ def make_gdrive_directory():
         command = 'STORED_DIR="data8/$JPY_USER"; \
                     echo "Creating Google Drive Directory named $STORED_DIR."; \
                     DIR_EXISTS="$(gdrive list | grep -i $STORED_DIR | wc -l)"; \
-                    [ $DIR_EXISTS -lt 1 ] && (RESULT=$(gdrive mkdir $STORED_DIR) && ID="$(echo $RESULT | cut -d " " -f 2)" && gdrive sync upload /home $ID) || echo "Directory already exists."; \
+                    [ $DIR_EXISTS -lt 1 ] && (RESULT=$(gdrive mkdir $STORED_DIR) && ID="$(echo $RESULT | cut -d " " -f 2)" && gdrive sync upload /home/' + HOME_SUBFOLDER_NAME + ' $ID) || echo "Directory already exists."; \
                     LOAD_DIRECTORY="$(gdrive list | grep -i $STORED_DIR | cut -c 1-28 | head -n 1)"; \
-                    gdrive sync upload /home $LOAD_DIRECTORY'
+                    gdrive sync upload /home/' + HOME_SUBFOLDER_NAME + ' $LOAD_DIRECTORY'
         p = Popen(command, stdout=PIPE, shell=True)
         output, err = p.communicate()
         sync_status = 'Created Sync Directory'
 
     return {
-        'status' : sync_status
+        'status': sync_status
     }
 
 def verify_gdrive_user(auth_code):
@@ -42,27 +45,27 @@ def sync_gdrive_directory():
     if drive_authenticated['authentication'] == "authenticated":
         command = 'STORED_DIR="data8/$JPY_USER"; \
                     LOAD_DIRECTORY="$(gdrive sync list | grep -i $STORED_DIR | cut -c 1-28 | head -n 1)"; \
-                    gdrive sync upload /home $LOAD_DIRECTORY; \
+                    gdrive sync upload /home/' + HOME_SUBFOLDER_NAME + ' $LOAD_DIRECTORY; \
                     echo "Syncing directory now."'
         p = Popen(command, stdout=PIPE, shell=True)
         output, err = p.communicate()
         sync_status = 'Successfully synced data to Google Drive'
 
     return {
-        'status' : sync_status
+        'status': sync_status
     }
 
 def check_gdrive_authenticated():
     p = Popen(['gdrive', 'about'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate('')
 
-    drive_authentication_url = str(output,'utf-8').split('\n')[2]
+    drive_authentication_url = str(output, 'utf-8').split('\n')[2]
 
     if 'http' not in drive_authentication_url:
         drive_authentication_url = "authenticated"
 
     return {
-        'authentication' : drive_authentication_url
+        'authentication': drive_authentication_url
     }
 
 def check_gdrive_last_sync_time():
@@ -76,7 +79,7 @@ def check_gdrive_last_sync_time():
     lastSyncTime = str(output, 'utf-8').split('\n')[0]
 
     return {
-        'lastSyncTime' : lastSyncTime
+        'lastSyncTime': lastSyncTime
     }
 
 class SyncHandler(IPythonHandler):
